b28dfec2bb1c92d128ebfb171779cf4a8102efee
santi bazan <santiago.n.bazan@outlook.com>
2023-09-19 11:36:25 -0300

    Ejercicio del dia 19/08

--- clase10.py
+++ clase10.py
@@ -1,13 +1,26 @@
 import funciones
 
 #ejercicio en clase 1
-num = int(input("Numeros a procesar:  "))
+#num = int(input("Numeros a procesar:  "))
 
-while(num!=0):
-    print(f"suma : {funciones.function(num)}")
-    num = int(input("Numeros a procesar:  "))
+#while(num!=0):
+#    print(f"suma : {funciones.function(num)}")
+#    num = int(input("Numeros a procesar:  "))
 
 #ejercicio de funciones
+
+def most(a,b):
+    if(a>b):
+        return a
+    else:
+        return b
+
+def least(a,b):
+    if(a<b):
+        return a
+    else:
+        return b
+
 x=int(input("Un numero: "))
 y=int(input("Otro numero: "))
 
--- funciones.py
+++ funciones.py
@@ -1,10 +1,10 @@
-def function(number):
-    add = 0
-    while (number!=0):
-        digit =number%10
-        add+=digit
-        number //= 10
-    return add   
+#def function(number):
+#    add = 0
+#    while (number!=0):
+#        digit =number%10
+#        add+=digit
+#        number //= 10
+#    return add   
     
 
 def most(a,b):
